cf96655c084d300de17a88101602746b8c9ca5ea
Eren Küçük <eren@mkbwebhoster.com>
2022-11-09 02:20:28 +0300

    Class Oluşturuldu.

--- lib/wuwisoft.class.php
+++ lib/wuwisoft.class.php
@@ -3,5 +3,29 @@
 namespace wuwisoft;
 
 class wuwisoft {
+	/**
+	 *
+	 * @param string $userCode
+	 */
+	private $userCode;
 
+	/**
+	 *
+	 * @param string $userName
+	 */
+	private $UserName;
+
+	/**
+	 *
+	 * @param string $userPassword
+	 */
+	private $UserPassword;
+
+
+
+	public function __construct(string $userCode,string $userName,string $userPassword) {
+		$this->userCode = $userCode;
+		$this->userName = $userName;
+		$this->userPassword = $userPassword;
+	}
 }
